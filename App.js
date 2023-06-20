7b3855e33367e5f47bdcd2d06a2ed24d8a1ce2bf
Vivian Zhang <vvynz12@gmail.com>
2023-06-20 00:09:57 -0400

    styling update

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={[styles.container, { flexDirection: "column" }]}>
+      <Text style={styles.purple}>hello moto</Text>
+      <View style={{ flex: 1, backgroundColor: 'red' }} />
+      <View style={{ flex: 2, backgroundColor: 'pink' }} />
+      <View style={{ flex: 1, backgroundColor: '#b0e1d7' }} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    // alignItems: "center",
+    // justifyContent: "center",
+    padding: 20,
+    marginTop: 25,
+  },
+  purple: {
+    color: "#ddbbff",
+    fontSize: 30,
   },
 });
